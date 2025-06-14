87baace56a979e7ac0a80ddbd77a3b97160e51f2
karol-brejna-i <karolbrejna@apache.org>
2025-06-14 08:31:03 +0000

    Add more debug info for book_spot.py

--- tidarator/spots/book_spot.py
+++ tidarator/spots/book_spot.py
@@ -4,6 +4,8 @@ from ..actions.action_base import ParkanizerActionBase
 from ..api.utils import str_to_date
 from ..log_config import get_logger
 
+import json
+
 logger = get_logger(__name__)
 
 
@@ -61,8 +63,9 @@ class BookSpot(ParkanizerActionBase):
             spots = [spots]
 
         spots_states = self.spot_manager.get_spots_state(zone_id, str_to_date(p['for_date']))
-        spot_ids = self._expand_spot_selection(zone_id, spots, spots_states)
+        logger.debug(f'Spot states: {json.dumps(spots_states)}')
 
+        spot_ids = self._expand_spot_selection(zone_id, spots, spots_states)
         logger.debug(f'Zone ID: {zone_id}; Spot IDs: {spot_ids}')
 
         result: dict[str, dict] = {'action': 'book_spot', 'request': p}
